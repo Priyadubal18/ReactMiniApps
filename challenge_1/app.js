be8c0e234bed1d20df2a94833fc24a7757efa7a4
Priyadubal18 <priya.dubal18@gmai.com>
2019-04-04 00:34:19 -0400

    Mostly completed the challenge_1

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,112 @@
+
+var text = document.getElementById('message');
+function initialize() {
+    var trelemnts = document.getElementsByClassName('row');
+    var init = new ticTacToe();
+    for (var i = 0; i < trelemnts.length; i++) {
+        trelemnts[i].innerHTML = '';
+        trelemnts[i].addEventListener('click', init.placePlayer.bind(init, trelemnts[i]));
+    }
+}
+function resetGame() {
+    initialize();
+    text.innerHTML = '';
+}
+
+var ticTacToe = function () {
+    this.board = [[0, 0, 0,],
+    [0, 0, 0,],
+    [0, 0, 0,]
+    ];
+    this.currentPlayer = 'X';
+    this.gameOver = false,
+        this.isEmptyBoard = function (row, col) {
+            return this.board[row][col] === 0
+        };
+    this.placePlayer = function (data) {
+        debugger;
+        let playerId = data.id.split('_');
+        let row = playerId[0];
+        let col = playerId[1];
+        //Check if board is empty
+        if (this.isEmptyBoard(row, col) && (!this.gameOver)) {
+            this.board[row][col] = this.currentPlayer;
+            this.updateDom(data, row, col);
+            this.checkWinnerPattern(row, col, this.currentPlayer);
+            this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+            // Check row pattern
+        }
+    };
+    this.updateDom = function (data, row, col) {
+        data.innerHTML = this.board[row][col];
+        //data.innerText = this.board[row][col];
+    };
+    this.checkWinnerPattern = function (row, col, value) {
+        if ((this.checkMajorDiagonalConfilct(value)) || (this.checkMinorDiagonalConfilct(value)) || (this.checkRowPattern(row, value)) || (this.checkColPattern(col, value))) {
+            this.gameOver = true;
+            text.innerHTML = `The winner is ${value}`;
+        } else if (this.checkDraw()) {
+            this.gameOver = true;
+            text.innerHTML = `Its a Draw Game`;
+        }
+
+    };
+    this.checkRowPattern = function (row, value) {
+        return this.board[row].every(function (tr) {
+            return tr === value;
+        });
+    };
+    this.checkColPattern = function (col, value) {
+        let colArray = [];
+        for (var i = 0; i < this.board[0].length; i++) {
+            colArray.push(this.board[i][col])
+        }
+        return colArray.every(function (tc) {
+            return tc === value;
+        })
+    };
+    this.checkMajorDiagonalConfilct = function (value) {
+        debugger;
+        var j = 0;
+        let majorDiagonalArray = [];
+        for (var i = 0; i < this.board[0].length; i++) {
+            majorDiagonalArray.push(this.board[i][j]);
+            j++;
+        }
+        return majorDiagonalArray.every(function (dr) {
+            return dr === value;
+        });
+    };
+    this.checkMinorDiagonalConfilct = function (value) {
+        debugger
+        var j = this.board[0].length - 1;
+        let minorDiagonalArray = [];
+        for (var i = 0; i < this.board[0].length; i++) {
+            minorDiagonalArray.push(this.board[i][j]);
+            j--;
+        }
+        return minorDiagonalArray.every(function (dr) {
+            return dr === value;
+        });
+    };
+    this.checkDraw = function () {
+        for (var i = 0; i < this.board.length; i++) {
+            for (j = 0; j < this.board.length; j++) {
+                if (this.board[i][j] === 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+}
+
+initialize();
+
+/*
+With Dom Elements
+
 var isX = false;
 var gameOver = false;
 var trelemnts = document.getElementsByClassName('row');
@@ -126,3 +235,4 @@ function checkDiagonal(row_col, value) {
 
 }
 
+*/
