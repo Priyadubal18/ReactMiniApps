0b60f9f9108c6cde1405d6561148f6af5a464465
Priyadubal18 <priya.dubal18@gmai.com>
2019-04-04 23:49:27 -0400

    Completed the front end

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
+app.use(express.static('client'));
+
+app.post('/upload_json', function (req, res) {
+
+    res.send('Hello world');
+});
+
+app.listen(port, () => console.log(`Miniapp create CSV is running on port 127.0.0.1:${port}`));
