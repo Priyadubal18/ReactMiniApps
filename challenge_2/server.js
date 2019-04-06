fe8aa60293c7d30a488c23b16cb111a29a001fef
Priyadubal18 <priya.dubal18@gmai.com>
2019-04-05 23:29:24 -0400

    Completed the form submission

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,12 +2,43 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.use(express.static('client'));
+app.use(express.json());
+app.use(express.urlencoded())
+app.set('view engine', 'ejs');
 
 app.post('/upload_json', function (req, res) {
-
-    res.send('Hello world');
+    let inputdata = JSON.parse(req.body.message);
+    let result = pasrJson(inputdata);
+    console.log(result);
+    res.send(result);
 });
+
+function pasrJson(data) {
+    var s = '';
+    Object.keys(data).forEach(key => {
+        if (!Array.isArray(data[key]) || ! typeof data[key] === 'object')
+            s += `${key}, `;
+    });
+
+    function parseProperties(obj) {
+        s += `\n`;
+        for (var key in obj) {
+            if (!Array.isArray(data[key]) || ! typeof data[key] === obj) {
+                s += `${obj[key]}, `;
+            } else {
+                if (Array.isArray(obj[key])) {
+                    for (var i = 0; i < obj[key].length; i++) {
+                        parseProperties(obj[key][i]);
+                    }
+                } else {
+                    parseProperties(obj[key]);
+                }
+            }
+        }
+    }
+    parseProperties(data);
+    return s;
+}
 
 app.listen(port, () => console.log(`Miniapp create CSV is running on port 127.0.0.1:${port}`));
