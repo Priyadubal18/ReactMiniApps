d4f37668cb1b7f8a395ac5c437c26ed78626740b
Priyadubal18 <priya.dubal18@gmai.com>
2019-04-06 15:07:47 -0400

    Completed BMR with new Index

---
+++ challenge_2/client/app.js
@@ -0,0 +1,19 @@
+
+
+// function sendJsonData(e) {
+//     let $form = $('#jsonForm');
+//     console.log($('form').serialize());
+//     e.preventDefault();
+//     $.ajax({
+//         method: 'POST',
+//         url: '/upload_json',
+//         data: $('form').serialize(),
+//         success: function (data) {
+//             console.log(data);
+//             $("#csvReport").html(data);
+//         }
+//     });
+// }
+
+// // ... more event handler code here
+
