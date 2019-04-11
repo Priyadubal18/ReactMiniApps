05d8a715b49d82c5145bcc6dd387f108dc869aaf
Priyadubal18 <priya.dubal18@gmai.com>
2019-04-10 23:22:55 -0400

    Added the logic to shoe component on button click

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,4 +1,32 @@
-var App = () => (
-    <div>Some cliche </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isForm1: false
+        }
+        this.chekoutClick = this.chekoutClick.bind(this);
+    }
+    chekoutClick() {
+        this.setState({
+            isForm1: true
+        });
+    }
+    render() {
+        return (
+            <div>
+                {this.state.isForm1 ? null : <input type="submit" value="Search" onClick={this.chekoutClick} />}
+                {this.state.isForm1 ? <F1 /> : null}
+            </div >
+
+        )
+    }
+}
+
+class F1 extends React.Component {
+    render() {
+        return (
+            <div>Hmm....</div>
+        );
+    }
+}
 ReactDOM.render(<App />, document.getElementById("app"));
